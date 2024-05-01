cda7658509b57dfd2580dce070f352007e452996
Alan <alanyoshiotara@gmail.com>
2024-05-01 14:52:13 -0300

    Testbench

--- src/tb.vhd
+++ src/tb.vhd
@@ -67,21 +67,32 @@ begin
     rst<='1'; wait for period_time;
     rst<='0'; wait for period_time;
 
-    
+    --addi r1, zero, 0x1001
     reg_sel_a<="000"; sel_imm<='1'; immediate <= "0001000000000001"; ula_sel<="000"; wait for period_time;
 
     reg_sel_wr<="001"; wr_en<='1'; wait for period_time;
     sel_imm<='0'; wr_en<='0';
     
+    --add r3, zero, 0x10A1
     reg_sel_a<="000"; sel_imm<='1'; immediate <= "0001000010100001"; ula_sel<="000"; wait for period_time;
 
     reg_sel_wr<="011"; wr_en<='1'; wait for period_time;
     sel_imm<='0'; wr_en<='0';
 
+    --add r4, r1, r2
+    reg_sel_a<="001"; reg_sel_b<="011"; ula_sel<="000"; wait for period_time;
+
+    reg_sel_wr<="100"; wr_en<='1'; wait for period_time;
+    wr_en<='0';
+
+    --See sum, or, not
     reg_sel_a<="001"; reg_sel_b<="011"; ula_sel<="000"; wait for period_time;
     reg_sel_a<="001"; reg_sel_b<="011"; ula_sel<="010"; wait for period_time;
     reg_sel_a<="001"; reg_sel_b<="011"; ula_sel<="100"; wait for period_time;
 
+    reg_sel_a<="100"; reg_sel_b<="011"; ula_sel<="000"; wait for period_time;
+    reg_sel_a<="001"; reg_sel_b<="011"; ula_sel<="001"; wait for period_time;
+    reg_sel_a<="100"; reg_sel_b<="100"; ula_sel<="001"; wait for period_time;
     wait;
   end process;
 end architecture;
